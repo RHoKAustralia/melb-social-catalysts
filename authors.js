2785211890fe9a6423c49fbe191ddcae85addd94
Yakov Khalinsky <yakov@therocketsurgeon.com>
2015-10-12 21:53:46 +1100

    Limit writers fetched to 5

--- authors.js
+++ authors.js
@@ -3,7 +3,7 @@ var fetch = require('./lib/fetch');
 var SQL = 'select users.name as name, users.image as bioImage, users.slug as userSlug, users.image, posts.published_at, ' +
     'posts.title as lastPostTitle, posts.slug as lastPostSlug from roles_users, users, posts ' +
     'where roles_users.role_id = 3 and users.status = \'active\' and roles_users.user_id = users.id and ' +
-    'users.id = posts.author_id group by users.name order by published_at desc';
+    'users.id = posts.author_id group by users.name order by published_at desc limit 5';
 
 var options = {
     dataLabel: 'writers',
